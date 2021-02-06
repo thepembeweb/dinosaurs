b345e80d6bfb2ad8f8c1befe2d24a11c2d70ee0b
Pemberai Sweto <pembeweb@yahoo.com>
2021-02-06 20:19:10 +0200

    feat: add dino functions

--- app.js
+++ app.js
@@ -27,3 +27,36 @@ const inputFeet = document.getElementById('feet');
 const inputInches = document.getElementById('inches');
 const inputName = document.getElementById('name');
 const inputWeight = document.getElementById('weight');
+    
+
+/**
+ * End Page Elements
+ * * Start Dino Functions
+ *
+ */
+
+
+// Create Dino Constructor
+function Dino(diet, fact, height, species, weight, where, when){
+    this.diet = diet;
+    this.fact = fact;
+    this.height = height;
+    this.species = species;
+    this.weight = weight;
+    this.where = where;
+    this.when = when;
+    this.comparisonResults = [];
+}
+
+// Create Dino Objects
+const getDinos = async () => {
+    try {
+        const response = await fetch('dino.json');
+        const dinos = await response.json();
+        return dinos.Dinos.map(dino => new Dino(dino.diet, dino.fact, dino.height, dino.species, dino.weight, dino.where, dino.when));
+    } catch(err) {
+        console.log(err);
+    }
+
+    return []
+}
